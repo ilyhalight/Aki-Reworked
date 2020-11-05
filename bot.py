f7fd3f425864e28fd14bc75286756985247638fd
Toil <62353659+ilyhalight@users.noreply.github.com>
2020-11-06 01:34:17 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,40 @@
+import discord
+import os
+from discord.ext import commands
+import config
+import clock
+from config import cogs_color,  settings
+prefix = settings['PREFIX']
+client = commands.Bot(command_prefix=prefix)
+client.remove_command('help')
+owner_id = settings['OWNER ID']
+@client.command()
+async def load(ctx, extensions):
+    if ctx.author.id == owner_id:
+        client.load_extension(f'cogs.{extensions}')
+        await ctx.send('Cogs is loaded...')
+    else:
+        await ctx.send('Вы не разработчик бота...')
+        
+@client.command()
+async def unload(ctx, extensions):
+    if ctx.author.id == owner_id:
+        client.unload_extension(f'cogs.{extensions}')
+        await ctx.send('Cogs is unloaded...')
+    else:
+        await ctx.send('Вы не разработчик бота...')
+        
+@client.command()
+async def reload(ctx, extensions):
+    if ctx.author.id == owner_id:
+        client.unload_extension(f'cogs.{extensions}')
+        client.load_extension(f'cogs.{extensions}')
+        await ctx.send('Cogs is restarted...')
+    else:
+        await ctx.send('Вы не разработчик бота...')
+        
+for filename in os.listdir('./cogs'): # Цикл перебирающий файлы в cogs
+    if filename.endswith('.py'): # еTсли файл кончается на .py, то это наш ког
+        client.load_extension(f'cogs.{filename[:-3]}') #командуем боту загрузить все расширения.
+        
+client.run (settings['TOKEN'])
---
+++ config.py
@@ -0,0 +1,18 @@
+settings = {
+	'TOKEN': 'Write your bot`s token here', #ur bot`s token
+	'BOT NAME': 'Aki', #ur bot`s name
+	'ID': 'Write your bot`s id here', #ur bot`s id
+	'OWNER ID': 551653500216672256, #ur id
+	'PREFIX': '$', #bot prefix
+	'OWNER':'Toil#6505', #ur name
+	'OWNER PING':'@Toil' #dosn`t work
+}
+
+cogs_color = {
+    'AVATAR COLOR': 0x0c0c0c,
+    'RANDOM COLOR': 0xca3a3a,
+	'RANDOM COLOR ERROR': 0xc9a638,
+	'TIME COLOR':0x4a8f62,
+	'WIKIPEDIA COLOR': 0x00ffff,
+ 	'WIKIPEDIA COLOR ERROR': 0xc9a638,
+}
