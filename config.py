e052dc2759df61abc46ef6d20233d11822b85b57
Toil <62353659+ilyhalight@users.noreply.github.com>
2020-11-06 19:30:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,22 @@ settings = {
 }
 
 cogs_color = {
-    'AVATAR COLOR': 0x0c0c0c,
-    'RANDOM COLOR': 0xca3a3a,
+	'AVATAR COLOR': 0x0c0c0c,
+	'RANDOM COLOR': 0xca3a3a,
 	'RANDOM COLOR ERROR': 0xc9a638,
-	'TIME COLOR':0x4a8f62,
+	'TIME COLOR': 0x4a8f62,
 	'WIKIPEDIA COLOR': 0x00ffff,
- 	'WIKIPEDIA COLOR ERROR': 0xc9a638,
+	'WIKIPEDIA COLOR ERROR': 0xc9a638,
+	'BOT COLOR ARG ERROR': 0xca3a3a,
+	'BOT COLOR ERROR': 0xc9a638,
+}
+
+quick_messages = {
+    #logs
+	'UNKNOWN ERROR LOG': '[Logs:error] Произошла неизвестная ошибка! |',
+ 	'UNKNOWN ERROR': 'Произошла неизвестная ошибка!',
+  	'BOT STATUS LOG': '[Logs:owner] Статус бота был успешно сменен на:'
+   #other
+	'COPYRIGHT RU': 'Copyright © 2020 Aki | Все права защищены.',
+ 	'COPYRIGHT EN': 'Copyright © 2020 Aki | All rights reserved.',
 }
