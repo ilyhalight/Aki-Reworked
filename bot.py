df49d4e3c4df2e4eeef049719e5479bade981bb3
Toil <62353659+ilyhalight@users.noreply.github.com>
2020-11-12 01:27:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import discord
 import os
 from discord.ext import commands
 import config
-import clock
 from config import cogs_color,  settings
 prefix = settings['PREFIX']
 client = commands.Bot(command_prefix=prefix)
